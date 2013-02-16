adc346a77a1975121cb96b47ff895a42c36dc0ac
Yogi Arjan <yogiarjan@gmail.com>
2013-02-16 07:30:04 +0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Garato::Application.config.secret_token = 'a2f454f7ca42a66b1e01d858f3c26bbf164c5af982935c726ab908418e21edd19adcc3e99c88e16fa5c702fdcf77c4afe04b7593ac438967c5ae0ca83dbd9355'
